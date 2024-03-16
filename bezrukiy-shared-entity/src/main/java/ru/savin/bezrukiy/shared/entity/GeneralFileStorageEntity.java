dba3f8ad3fccf91ce02ca10c8c4018b381dd7e05
ddd <ya@dssssss.ru>
2024-03-17 01:23:08 +0300

    Добавлен FileStorage и интерфейс для работы с несколькми файлами

--- bezrukiy-shared-entity/src/main/java/ru/savin/bezrukiy/shared/entity/GeneralFileStorageEntity.java
+++ bezrukiy-shared-entity/src/main/java/ru/savin/bezrukiy/shared/entity/GeneralFileStorageEntity.java
@@ -2,10 +2,9 @@ package ru.savin.bezrukiy.shared.entity;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import org.hibernate.annotations.GenericGenerator;
 
-import javax.persistence.Column;
-import javax.persistence.Id;
-import javax.persistence.MappedSuperclass;
+import javax.persistence.*;
 import java.util.UUID;
 
 /**
@@ -17,7 +16,7 @@ import java.util.UUID;
 public abstract class GeneralFileStorageEntity extends GeneralConceptEntity {
 
     @Id
+    @GeneratedValue
     @Column(name = "file_id")
-    private UUID fileId = UUID.randomUUID();
+    private UUID fileId;
 }
-
